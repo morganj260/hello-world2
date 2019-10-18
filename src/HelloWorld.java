f7566756d9eea341538ac795bc829bb2132a313e
Morgan Jarvis <morganj260@gmail.com>
2019-10-18 16:53:14 -0700

    main method that prints hello world

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,13 @@
+/**
+ * @author Morgan Jarvis
+ * @version 2019.10.18
+ */
+
 public class HelloWorld {
+
+    public static void main(String[] arg){
+        System.out.println("helloworld");
+
+    }
+
 }
